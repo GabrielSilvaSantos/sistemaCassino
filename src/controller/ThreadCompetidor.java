fecedef73f826b5930c51487860738ddffb65e07
Gabriel da Silva Santos <GabrielSilvaSantos@users.noreply.github.com>
2020-04-14 21:42:41 -0300

    v2

--- src/controller/ThreadCompetidor.java
+++ src/controller/ThreadCompetidor.java
@@ -1,26 +1,42 @@
+/*
+ @author gabriels
+ @date 14/04/2020
+*/
 package controller;
 
 import java.util.Random;
 
 public class ThreadCompetidor extends Thread {
 
+	// Variáveis para identificação e controle dos jogadores
 	static int qtdVitoriosos;
-	private Random a = new Random();
 	private int idJogador;
+	private Random a = new Random();
 
+	// Construtor da classe junto a inicialização da thread
 	public ThreadCompetidor(int idJogador) {
+
 		this.idJogador = idJogador;
 		this.start();
 	}
 
+	// Chamada do método ao iniciar a thread
 	public void run() {
+
 		competir();
 	}
 
+	// Método para lançamento dos dados
 	public boolean jogarDados() {
-		int dadoA = 0, dadoB = 0;
+
+		int dadoA = 0;
+		int dadoB = 0;
+
+		// Definição de valores aleatórios nos dados
 		dadoA = a.nextInt(6) + 1;
 		dadoB = a.nextInt(6) + 1;
+
+		// Validação da regra de pontuação
 		if (dadoA + dadoB == 7 || dadoA + dadoB == 11) {
 			return true;
 		} else {
@@ -28,7 +44,9 @@ public class ThreadCompetidor extends Thread {
 		}
 	}
 
+	// Método para controle da pontuação e dos jogadores
 	public void competir() {
+
 		int pontuacao = 0;
 		do {
 			try {
@@ -36,24 +54,29 @@ public class ThreadCompetidor extends Thread {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			// Soma pontuação
 			if (jogarDados() == true) {
 				pontuacao++;
 			}
 		} while (pontuacao < 5);
+
+		//
 		qtdVitoriosos++;
 
+		// Controla e exibe os ganhadores e perdedores
 		switch (qtdVitoriosos) {
 		case 1:
-			System.out.println("Jogador n°" + idJogador + " ganhou R$ 5000,00" );
+			System.out.println("JOGADOR N°" + idJogador + " ganhou R$ 5000,00");
 			break;
 		case 2:
-			System.out.println("Jogador n°" + idJogador + " ganhou R$ 4000,00" );
+			System.out.println("JOGADOR N°" + idJogador + " ganhou R$ 4000,00");
 			break;
 		case 3:
-			System.out.println("Jogador n°" + idJogador + " ganhou R$ 3000,00" );
+			System.out.println("JOGADOR N°" + idJogador + " ganhou R$ 3000,00");
 			break;
 		default:
-			System.out.println("Jogador n°" + idJogador + " não ganhou nada! " );
+			System.out.println("JOGADOR N°" + idJogador + " não ganhou nada! ");
+
 		}
 	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,11 +1,16 @@
+/*
+ @author gabriels
+ @date 14/04/2020
+*/
 package view;
 
 import controller.ThreadCompetidor;
 
 public class Principal {
 	public static void main(String[] args) {
-		
-		for (int jogador = 1; jogador < 11; jogador++) {
+
+		// Chamada da thread contendo 10 jogadores
+		for (int jogador = 1; jogador <11; jogador++) {
 			Thread jogo = new ThreadCompetidor(jogador);
 		}
 	}
